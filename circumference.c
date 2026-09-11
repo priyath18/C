bb7e02744ba0fae2f0f033f0d8d97d755d9d8fbb
priyath18 <priyathakr18@gmail.com>
2026-09-11 12:56:42 +0530

    Add C files

---
+++ circumference.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+    float r=5,c;
+    c=2*PI*r;
+    printf("The circumference of the circle is: %.2f",c);
+    return 0;
+
+}
---
+++ classwork.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x=20, y=25 ;
+    printf("The x<y: %d",x<y);
+    printf("The x>y: %d",x>y);
+    printf("The x<=y: %d",x<=y);
+    printf("The x>=y: %d",x>=y);
+    printf("The x==y: %d",x==y);
+    printf("The x!=y: %d",x!=y);
+return 0;
+
+}
