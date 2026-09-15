23d3856c0abeaf18de8ca9c2d03218b727ec48cb
priyath18 <priyathakr18@gmail.com>
2026-09-15 12:31:41 +0530

    Add C files

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the value of x");
+    scanf("%d", &x);
+    if (x > 0)
+    {
+     printf("The number is positive");
+    }
+    else if (x < 0)
+    {
+     printf("The number is negative");
+    }
+    else
+    {
+     printf("The number is zero");
+    }
+    return 0;
+}
