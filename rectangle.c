9c4fd385b8bf8aec0f49e00e3215e4a2b6c75ccb
priyath18 <priyathakr18@gmail.com>
2026-09-21 13:30:18 +0530

    Add C files

---
+++ rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float length, breadth, area;
+
+    printf("Enter length and breadth: ");
+    scanf("%f %f", &length, &breadth);
+
+    area = length * breadth;
+
+    printf("Area = %.2f", area);
+    return 0;
+}
